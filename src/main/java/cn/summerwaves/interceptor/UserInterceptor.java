6a5a058aee4dfb15876c2919a2ef515b9a913709
summerwaves <z7251007@163.com>
2018-03-15 15:41:53 +0800

    上传

--- src/main/java/cn/summerwaves/interceptor/UserInterceptor.java
+++ src/main/java/cn/summerwaves/interceptor/UserInterceptor.java
@@ -15,6 +15,7 @@ public class UserInterceptor implements HandlerInterceptor {
     private Boolean debug;
 
 
+    @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         String test = httpServletRequest.getParameter("test");
         String path = httpServletRequest.getServletPath();
@@ -22,10 +23,12 @@ public class UserInterceptor implements HandlerInterceptor {
         return true;
     }
 
+    @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
 
     }
 
+    @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
 
     }
